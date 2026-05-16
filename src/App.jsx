b418c6c6aedcd33d996ca680b598d245763ee7df
copepb <qwwo5015@gmail.com>
2026-05-16 17:51:00 +0900

    영화 리스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,44 @@ const App = () => {
   
   return (
     <div className="h-screen">
-      <div id="Header" className="h-30 bg-amber-950 flex justify-center items-center">
-        <div className="text-white text-4xl font-bold">Movie List</div>
+      <div id="Header" className="h-[150px] bg-amber-950 flex justify-center items-center">
+        <div className="text-white text-5xl font-bold">Movie List</div>
+      </div>
+
+      <div id="Container">
+        <div className="px-5 py-10">
+          <div className="flex justify-between">
+            <div id="WatchedMovies" className="w-[370px] min-h-screen bg-gray-300">
+              <div className="p-5 flex justify-center">
+                <div className="text-2xl font-bold">시청한 영화 목록</div>
+              </div>
+            </div>
+
+            <div id="MovieList" className="w-[450px] h-[700px] bg-gray-500 overflow-y-scroll p-4">
+              {movies.map((movie) => (
+                <div key={movie.id} className="bg-white border-2 p-2 mb-3 border-black text-center">
+                  <div className="font-black">
+                    {movie.title}
+                  </div>
+                  <div>
+                    {movie.description}
+                  </div>
+                  <div className="flex justify-around">
+                    <button className="bg-gray-300 border-1 p-1">시청한 영화 담기</button>
+                    <button className="bg-gray-300 border-1 p-1">볼 영화 담기</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div id="MovieToWatch" className="w-[370px] min-h-screen bg-gray-300">
+              <div className="p-5 flex justify-center">
+                <div className="text-2xl font-bold">볼 영화 목록</div>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   )
